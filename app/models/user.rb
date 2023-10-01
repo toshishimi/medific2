4d394ca90871f53066821fa53fe8e3019136959b
toshishimi <sc.104.k@gmail.com>
2023-10-01 14:53:37 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :name,        format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :name_kana,   format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birth_date
+  end
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  
 end

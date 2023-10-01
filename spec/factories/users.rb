3d97e1d326881b4805a6cf7a798e6718617dcfa9
toshishimi <sc.104.k@gmail.com>
2023-10-01 21:10:09 +0900

    ユーザーのFaker変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -8,7 +8,7 @@ FactoryBot.define do
     name                  {person.last.kanji }
     name_kana             {person.last.katakana}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1)}
     password_confirmation {password}
     birth_date            {Faker::Date.backward}
     

0ff499e1e9eef24a33d1f8f086b9b3bbda3d679b
toshishimi <sc.104.k@gmail.com>
2023-10-02 20:53:28 +0900

    ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'date_hospitals/index'
   devise_for :users
   root to: "date_hospitals#index"
   resources :date_hospitals, only: [:index, :new, :create] do
-    resources :medications, only: :show
+    resources :medications, only: [:show, :edit, :update]
   end
 end
